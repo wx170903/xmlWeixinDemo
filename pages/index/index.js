4e7d517f8949ca42eeb244bb6c95c147317c4b87
xianyili <384313798@qq.com>
2017-12-31 17:38:16 +0800

    项目构建完成

---
+++ pages/index/index.js
@@ -0,0 +1,13 @@
+Page({
+  data: {
+    imgUrls: [
+      "http://img0.imgtn.bdimg.com/it/u=880477421,2302497799&fm=27&gp=0.jpg",
+      "http://img5.imgtn.bdimg.com/it/u=1741138662,3408053349&fm=27&gp=0.jpg",
+      "http://img4.imgtn.bdimg.com/it/u=2930540385,1562266871&fm=27&gp=0.jpg",
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 2000,
+    duration: 1000
+  },
+})
