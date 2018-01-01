e14136717ca45b7490f4d2d94b2c572fe2ab6854
xianyili <384313798@qq.com>
2018-01-01 19:03:45 +0800

    商品分类

--- app.js
+++ app.js
@@ -19,8 +19,8 @@ App({
           // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
           wx.getUserInfo({
             success: res => {
-              console.log(res);
-              console.log('得到用户信息成功')
+              // console.log(res);
+              // console.log('得到用户信息成功')
               // 可以将 res 发送给后台解码出 unionId
               this.globalData.userInfo = res.userInfo
 
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,3 +1,4 @@
+const app = getApp();
 Page({
   data: {
     imgUrls: [
@@ -9,5 +10,40 @@ Page({
     autoplay: true,
     interval: 2000,
     duration: 1000
+  },
+  changeTo:(event)=>{
+    console.log(event);
+    let where = event.currentTarget.dataset.where;
+    console.log(where);
+    if(where === 'orderRood'){
+      wx.switchTab({
+        url: '/pages/${where}/${where}',
+      })
+    }else{
+      wx.navigateTo({
+        url: '/pages/class/${where}/${where}',
+      });
+    }
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    // 加载的时候从easymoc获取全局数据
+    wx.request({
+      url: 'http://www.easy-mock.com/mock/5a1ffb42583969285ab22bb7/orderOnline/orderOnline',
+      complete: res => {
+        console.log(res);
+        app.globalData.classifyList = res.data;
+        console.log(app.globalData.classifyList);
+        this.setData({
+          classifyList: app.globalData.classifyList.myTrump,
+          newGoods: app.globalData.classifyList.newGoods,
+          classicGoods: app.globalData.classifyList.classicGoods,
+          components: app.globalData.classifyList.components,
+          headLines: app.globalData.classifyList.headLines,
+        });
+      },
+    });
   },
 })
